86d8e067dea4af153e70fc231abc8f6155c791f2
Amodhx <amxdhnanditha@gmail.com>
2024-07-21 22:42:30 +0530

    #navigation Home done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { AllocateComponent } from './pages/allocate/allocate.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     {
         path:"allocate",
         component:AllocateComponent
-    }
+    },
+    {
+        path:"home",
+        component:HomeComponent
+    },
+    
 ];
