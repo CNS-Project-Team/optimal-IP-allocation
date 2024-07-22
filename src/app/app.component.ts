d47c174b804a5c93b7d5d0367eb0b08a436ecd63
LRLayan <rameshlayan4@gmail.com>
2024-07-22 12:46:47 +0530

    home components path configure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './pages/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'ipcal-app';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { AllocateComponent } from './pages/allocate/allocate.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     {
-        path:"allocate",
-        component:AllocateComponent
+        path:'', component:HomeComponent
+    },
+    {
+        path:"home", component:HomeComponent
+    }
+    ,
+    {
+        path:"allocate", component:AllocateComponent
     }
 ];
