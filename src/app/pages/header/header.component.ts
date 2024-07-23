00ddd4d2372dfacabe1f12a69a7a76926db9ece7
LRLayan <rameshlayan4@gmail.com>
2024-07-23 16:46:56 +0530

    rule logic and import common modul , Router and NavigationEnd , filter ,subscription

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; //import
+import { Router, NavigationEnd } from '@angular/router'; // Import Router and NavigationEnd
+import { filter } from 'rxjs/operators'; // Import filter operator
+import { Subscription } from 'rxjs'; // Import Subscription
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],//import common module
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  activeLink: string | null = null;
+
+  setActiveLink(link: string) {
+    this.activeLink = link;
+    console.log("set",link);
+  }
+
+  constructor(private router: Router) {
+    // Track route changes to update activeLink
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.updateActiveLink();
+    });
+  }
+
+  updateActiveLink() {
+    this.activeLink = this.router.url.substring(1); // Remove leading '/'
+    console.log("update",this.activeLink);
+    
+  }
 }
