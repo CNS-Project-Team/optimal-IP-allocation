cab1ff12e61ffee1872eb0ad21bebac6104977e6
LRLayan <rameshlayan4@gmail.com>
2024-07-23 16:44:01 +0530

    import common module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './pages/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { CommonModule } from '@angular/common'; //import
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,HomeComponent],
+  imports: [RouterOutlet,HeaderComponent,HomeComponent,CommonModule], //common module , home module
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
