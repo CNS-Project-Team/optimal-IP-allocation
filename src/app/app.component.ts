10fb16fe3f21f753672f594e93bae82ab5ae28f6
LRLayan <rameshlayan4@gmail.com>
2024-07-24 15:06:36 +0530

    remove import home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './pages/header/header.component';
-import { HomeComponent } from './pages/home/home.component';
 import { CommonModule } from '@angular/common'; //import
+import { FooterComponent } from './pages/footer/footer.component'; //import
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,HomeComponent,CommonModule], //common module , home module
+  imports: [RouterOutlet, HeaderComponent, CommonModule, FooterComponent], //common module ,,footer
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
